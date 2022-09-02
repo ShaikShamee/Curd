b978db27ddc9bdbd5b4b794b9233e516dfec9dd1
ShaikShamee <shameema.shaik78@gmail.com>
2022-09-02 17:40:59 +0530

    inserted

---
+++ Practice/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    public class Hello
+    {
+        public void sayHello() { Console.WriteLine("Hello First Namespace"); }
+
+    }
+}
+    namespace Second
+    {
+        public class Hello
+        {
+            public void sayHello() { Console.WriteLine("Hello Second Namespace"); }
+        }
+    }
---
+++ Practice/ReferenceandValueType.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    public class ReferenceandValueType
+    {
+        //static void ChangeValue(int x)
+        //{
+        //    x = 200;
+
+        //    Console.WriteLine(x);
+        //}
+
+        //static void Main()
+        //{
+        //    int i = 100;
+
+        //    Console.WriteLine(i);
+
+        //    ChangeValue(i);
+
+        //    Console.WriteLine(i);
+        //}
+
+        public class Student
+        {
+
+            public string StudentName { get; set; }
+
+        }
+
+        static void ChangeReferenceType(Student std2)
+        {
+            std2.StudentName = "Steve";
+        }
+
+        static void Main(string[] args)
+        {
+            Student std1 = new Student();
+            std1.StudentName = "Bill";
+
+            ChangeReferenceType(std1);
+
+            Console.WriteLine(std1.StudentName);
+        }
+    }
+
+
+}
